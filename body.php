852b43f3599ab4f1a3b2e34160a59512735c5ee7
AYESHA SIDDIKA <109195459+stuBBornButterfly@users.noreply.github.com>
2024-04-30 14:18:54 +0600

    Add files via upload

---
+++ body.php
@@ -0,0 +1,109 @@
+<div class="main main-raised">
+        <div class="container mainn-raised" style="width:100%;">
+  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+   
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="./product_images/slider-1.jpg" alt="Los Angeles" style="width:25%;">
+        
+      </div>
+
+      <div class="item">
+        <img src="./product_images/slider-2.jpg" style="width:30%;">
+        
+      </div>
+    
+      <div class="item">
+        <img src="./product_images/slider-3.jpg" alt="New York" style="width:25%;">
+        
+      </div>
+      <div class="item">
+        <img src="./product_images/slider-4.jpg" alt="New York" style="width:10%;">
+        
+      </div>
+      
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only" >Previous</span>
+    </a>
+    <a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+     
+
+
+		<!-- SECTION -->
+		<div class="section mainn mainn-raised">
+		
+		
+			<!-- container -->
+			<div class="container">
+			
+				<!-- row -->
+				<div class="row">
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="men.ph"><div class="shop">
+							<div class="shop-img">
+								<img src="./product_images/product-2.jpg" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Men's<br>Collection</h3>
+								<a href="men.php" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="women.php"><div class="shop">
+							<div class="shop-img">
+								<img src="./product_images/product-11.jpg" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Women's<br>Collection</h3>
+								<a href="women.php" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="unisex.php"><div class="shop">
+							<div class="shop-img">
+								<img src="./product_images/product-10.jpg" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Unisex<br>Collection</h3>
+								<a href="unisex.php" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+                            </div></a>
+					</div>
+					<!-- /shop -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+		  
+		
+
+		<!-- SECTION -->
+
+		<!-- /SECTION -->
+</div>
---
+++ women.php
@@ -0,0 +1,98 @@
+<?php
+include "db.php";
+include 'header.php';
+// Check if the form is submitted
+
+    // Create the SQL query
+    $category = 2;
+
+    // Create the SQL query without the product_keywords condition
+    $sql = "SELECT * FROM products WHERE product_cat = ?";
+
+    // Prepare and bind parameters to the SQL statement
+    $stmt = $con->prepare($sql);
+
+    if (!$stmt) {
+        die('Error preparing statement: ' . $con->error);
+    }
+
+    $stmt->bind_param("i", $category);
+
+    // Execute the query
+    if (!$stmt->execute()) {
+        die('Error executing query: ' . $stmt->error);
+    }
+
+    // Fetch the results
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+        // Output data of each row
+        while($row = $result->fetch_assoc()) {
+            $cat_query = "SELECT cat_title FROM categories WHERE cat_id = ?";
+            $cat_stmt = $con->prepare($cat_query);
+            $cat_stmt->bind_param("i", $row['product_cat']);
+            $cat_stmt->execute();
+            $cat_result = $cat_stmt->get_result();
+            $category_name = $cat_result->fetch_assoc()['cat_title'];
+
+            // Display product with category name
+            $pro_id    = $row['product_id'];
+            $pro_cat   = $category_name; // Use fetched category name
+            $pro_brand = $row['product_brand'];
+            $pro_title = $row['product_title'];
+            $pro_price = $row['product_price'];
+            $pro_image = $row['product_image'];
+          
+            
+			echo "
+                
+                         <div class='col-md-3'>
+								<a href='product.php?p=$pro_id'><div class='product'>
+									<div class='product-img'>
+										<img src='product_images/$pro_image' style='max-height: 170px;' alt=''>
+										<div class='product-label'>
+							
+										</div>
+									</div></a>
+									<div class='product-body'>
+										
+										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
+                                        <h3 class='product-name header-cart-item-name'>$category_name</a></h3>
+										<h4 class='product-price header-cart-item-info'>$pro_price</h4>
+										<div class='product-rating'>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+										</div>
+										<div class='product-btns'>
+											<button class='add-to-wishlist'><i class='fa fa-heart-o'></i><span class='tooltipp'>add to wishlist</span></button>
+											<button class='add-to-compare'><i class='fa fa-exchange'></i><span class='tooltipp'>add to compare</span></button>
+											<button class='quick-view'><i class='fa fa-eye'></i><span class='tooltipp'>quick view</span></button>
+										</div>
+									</div>
+									<div class='add-to-cart'>
+										<button pid='$pro_id' id='product' class='add-to-cart-btn block2-btn-towishlist' href='#'><i class='fa fa-shopping-cart'></i> add to cart</button>
+									</div>
+								</div>
+                                </div>
+
+			";
+        }
+    } else {
+        echo "0 results";
+    }
+
+    // Close the statement
+    $stmt->close();
+    // Close the connection
+    $con->close();
+
+?>
+<div>
+<?php 
+
+include "footer.php";
+?>
+</div
